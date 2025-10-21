44b0ea243f4b4e44b35b4218366d06ba31e30b08
Luke Pickering <luketpickering@gmail.com>
2025-10-21 08:29:25 +0000

    wrong constructor signature for file reader... lets see if fixing it helps

--- app/gen_srproxy.cxx
+++ app/gen_srproxy.cxx
@@ -901,7 +901,7 @@ PYBIND11_MODULE(py{1}, m) {{
 
     (*out_pyb) << fmt::format(R"(
 py::class_<ProxyFileReader<{0}>>(m, "{1}FileReader")
-      .def(py::init<std::string, std::vector<std::string> const &>())
+      .def(py::init<std::string const &, std::vector<std::string> const &>())
       .def(
           "entries",
           [](ProxyFileReader<{0}> &s) {{
